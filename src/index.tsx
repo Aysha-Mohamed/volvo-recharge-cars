b6c9f8c80d77c06273b3f316a0a5328ec34492a3
Aysha <42186490+Aysha-Mohamed@users.noreply.github.com>
2023-09-05 14:54:57 +0200

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StyleProvider,ThemePicker } from 'vcc-ui';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StyleProvider>
+    <ThemePicker variant="light">
+      <App />
+    </ThemePicker>
+  </StyleProvider>
+    
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
